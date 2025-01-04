e54a95e0e6753dbe008c88fb928c5cec2c6a0f88
Swam Htet Aung <swamhtetaungg@gmail.com>
2025-01-04 18:03:28 +0700

    setup zod + react hook form and add validations for sign up page

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,60 +1,135 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
 
+const SignInSchema = z.object({
+  fullName: z.string(),
+  email: z.string().email(),
+  password: z.string().min(8),
+  agree: z
+    .boolean()
+    .optional()
+    .refine((value) => value === true, {
+      message: "You must agree to the terms and conditions",
+    }),
+});
+
+type SignInFormFields = z.infer<typeof SignInSchema>;
+
 const SignInPage = () => {
+  const form = useForm<SignInFormFields>({
+    resolver: zodResolver(SignInSchema),
+  });
+
+  const onSubmit: SubmitHandler<SignInFormFields> = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div className="py-10 px-6 space-y-6">
-      <h2 className="text-betterFinance-primary text-xl font-semibold">
-        Sign Up
-      </h2>
+    <Form {...form}>
+      <form
+        className="py-10 px-6 space-y-6"
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
+        <h2 className="text-betterFinance-primary text-xl font-semibold">
+          Sign Up
+        </h2>
 
-      <div className="grid w-full items-center gap-1.5">
-        <Label htmlFor="name">Full Name</Label>
-        <Input type="text" id="name" />
-      </div>
+        <FormField
+          control={form.control}
+          name="fullName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Full Name</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-      <div className="grid w-full items-center gap-1.5">
-        <Label htmlFor="email">Email</Label>
-        <Input type="email" id="email" />
-      </div>
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input type="email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-      <div className="grid w-full items-center gap-1.5">
-        <Label htmlFor="password">Password</Label>
-        <Input type="password" id="password" />
-      </div>
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-      <div className="flex justify-between items-center">
-        <div className="flex space-x-2">
-          <Checkbox id="agree" />
-          <Label htmlFor="agree">
-            I certify that I am 18 years of age or older, and agree to the{" "}
-            <span className="text-sm text-betterFinance-tertiary hover:underline">
-              User Agreement
-            </span>{" "}
-            and{" "}
-            <span className="text-sm text-betterFinance-tertiary hover:underline">
-              Privacy Policy
-            </span>
-          </Label>
+        <FormField
+          control={form.control}
+          name="agree"
+          render={({ field }) => (
+            <div>
+              <FormItem className="flex flex-row items-start space-x-3 space-y-0">
+                <FormControl>
+                  <Checkbox
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                  />
+                </FormControl>
+                <div className="space-y-1 leading-none">
+                  <FormLabel className="text-neutral-500 dark:text-neutral-400">
+                    I agree to the User Agreement and Privacy Policy
+                  </FormLabel>
+                </div>
+              </FormItem>
+              <FormMessage />
+            </div>
+          )}
+        />
+
+        <Button className="w-full">Sign Up</Button>
+
+        <div className="space-x-1 flex items-center">
+          <span className="text-sm">Already have an account?</span>
+          <Link
+            href="sign-in"
+            className="text-sm text-betterFinance-tertiary hover:underline"
+          >
+            Sign In
+          </Link>
         </div>
-      </div>
-
-      <Button className="w-full">Sign Up</Button>
-
-      <div className="space-x-1 flex items-center">
-        <span className="text-sm">Already have an account?</span>
-        <Link
-          href="sign-in"
-          className="text-sm text-betterFinance-tertiary hover:underline"
-        >
-          Sign In
-        </Link>
-      </div>
-    </div>
+      </form>
+    </Form>
   );
 };
 
