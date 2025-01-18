b97b03a6a0ed4cfb4dd32478fa93305f7fb25f7c
Swam Htet Aung <swamhtetaungg@gmail.com>
2025-01-18 14:20:53 +0700

    refactor finance score component

--- src/app/(dashboard)/dashboard/components/FinanceScore.tsx
+++ src/app/(dashboard)/dashboard/components/FinanceScore.tsx
@@ -1,4 +1,5 @@
 import { Progress } from "@/components/ui/progress";
+import { Ellipsis } from "lucide-react";
 import React from "react";
 
 const FinanceScore = () => {
@@ -8,15 +9,20 @@ const FinanceScore = () => {
         "flex-1 bg-slate-200 border rounded-3xl p-4 space-y-2 dark:border-betterFinance-primary flex flex-col justify-between"
       }
     >
-      <h1 className="font-bold text-betterFinance-primary text-2xl">
-        Finance Score
-      </h1>
+      <div className="flex items-center justify-between">
+        <h1 className="font-bold text-betterFinance-primary text-xl">
+          Finance Score
+        </h1>
+        <button>
+          <Ellipsis className="text-neutral-700" size={20} />
+        </button>
+      </div>
 
-      <div>
+      <div className="space-y-1">
         <p className="text-sm text-neutral-500">Finance Quality</p>
         <div className="space-y-4">
           <div className="flex justify-between">
-            <h1 className="font-extrabold text-betterFinance-primary text-3xl">
+            <h1 className="font-bold text-betterFinance-primary text-3xl">
               Excellent
             </h1>
             <h1 className="text-betterFinance-primary text-3xl">92%</h1>
